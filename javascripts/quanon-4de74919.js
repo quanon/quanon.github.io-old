08b0a28bfe2b8f97a61c1123e831080176fe2203
Shinpei Noda <noda@haw.co.jp>
2014-01-18 00:52:14 +0900

    "Automated commit at 2014-01-17 15:52:14 UTC by middleman-deploy 0.1.4"

---
+++ javascripts/quanon-4de74919.js
@@ -0,0 +1,1 @@
+!function(){$(document).ready(function(){return $(".title__logo").balloon({contents:"トゥットゥルー<br />かのしぃです★",position:"right",classname:"title__logo__balloon",offsetX:10,css:{opacity:1,boxShadow:0}})})}.call(this);
