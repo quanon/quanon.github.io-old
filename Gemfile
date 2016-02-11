937b9e1d72268edde7d43ce6f32a51f748c475a4
noda <noda@haw.co.jp>
2016-02-11 17:29:13 +0900

    Middleman のバージョンを更新した。

--- Gemfile
+++ Gemfile
@@ -1,12 +1,9 @@
+ruby '2.3.0'
 source 'https://rubygems.org'
 
-gem 'middleman', '~> 3.3.7'
-
-# Live-reloading plugin
-gem 'middleman-livereload', '~> 3.3.3'
-
-gem 'bootstrap-sass', '~> 3.3.1'
-gem 'middleman-sprockets', '~> 3.3.10'
-gem 'jquery-middleman', '~> 3.0.4'
-
-gem 'middleman-deploy', '~> 0.3.0'
+gem 'middleman', '~> 4.1.1'
+gem 'middleman-livereload', '~> 3.4.6'
+gem 'middleman-compass', '~> 4.0.1'
+gem 'jquery-middleman', '~> 3.1.2'
+gem 'bootstrap-sass', '~> 3.3.6'
+gem 'middleman-sprockets', '~> 4.0.0.rc.1'
--- config.rb
+++ config.rb
@@ -1,45 +1,33 @@
 ###
-# Compass
-###
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
 # Page options, layouts, aliases and proxies
 ###
 
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
+page '/*.xml', layout: false
+page '/*.json', layout: false
+page '/*.txt', layout: false
 
-# Proxy pages (http://middlemanapp.com/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
-#  :which_fake_page => "Rendering a fake page with a local variable" }
+# With alternative layout
+# page "/path/to/file.html", layout: :otherlayout
+
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
+
+# General configuration
+
+# Reload the browser automatically whenever files change
+configure :development do
+  activate :livereload
+end
 
 ###
 # Helpers
 ###
 
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Reload the browser automatically whenever files change
-# activate :livereload
-
 # Methods defined in the helpers block are available in templates
-
 module MiddlemanHelper
   def favorite_items(title, key, is_divided = true)
     locals = { title: title, items: key, is_divided: is_divided }
@@ -64,34 +52,11 @@ helpers do
   include MiddlemanHelper
 end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
+  # Minify CSS on build
   activate :minify_css
 
   # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
-  activate :asset_hash
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Or use a different image path
-  # set :http_prefix, "/Content/images/"
 end
-
-activate :deploy do |deploy|
-  deploy.method = :git
-  deploy.branch = :master
-  deploy.build_before = true
-end
-
-I18n.enforce_available_locales = false
